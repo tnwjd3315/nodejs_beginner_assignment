9169cfd5639d16da8af09295d25a3e29f8f3db42
Sujeong Kim <miysj3315@gmail.com>
2022-12-16 17:37:17 +0900

    전체/특정 게시글 조회 분리, 게시글 put 추가,

--- routes/posts.js
+++ routes/posts.js
@@ -6,12 +6,17 @@ const router = express.Router()
 
 const Posts = require("../schemas/post.js")
 
-// localhost:3000/api/posts GET Method
+// localhost:3000/api/posts GET Method 전체 게시글 조회
 router.get("/posts", async(req,res) => {
   const posts = await Posts.find({})
   res.json({posts})
-  console.log(`post id 값:${Types.ObjectId(posts._id)}`)
-  console.log(`posts 값:${posts}`)
+})
+
+// localhost:3000/api/posts GET Method 특정 게시글 조회
+router.get("/posts/:postsId", async(req,res) => {
+  const {postsId} = req.params
+  const post = await Posts.find({postsId: Number(postsId)})
+  res.json({post})
 })
 
 // localhost:3000/api/posts POST Method
@@ -22,14 +27,30 @@ router.post("/posts", async(req,res) => {
 })
 
 // localhost:3000/api/posts PUT Method
-router.put("/posts/:_id", async(req,res) => {
-  const {_id} = req.params
+router.put("/posts/:postsId", async(req,res) => {
+  const {postsId} = req.params
   const {title, content} = req.body
-  await Posts.updateOne(
-    {$set: {title, content}}
-    )
-    res.json({success:true})
+
+  //레코드의 개수(쿼리 결과 리절트의 개수)로 비밀번호 일치여부를 판단
+  //몽고 디비에 일치하는 document가 있는지의 여부로 판단.
+  //만약 editPosts가 있다면 비밀번호 일치, 그렇지 않으면 비밀번호 불일치
+  const editPosts = await Posts.find({postsId, password})
+  const pw = editPosts[0].password
+
+  try{
+    if (!pw || !title.length) {
+      return res.return(400).json({
+        success:false,
+        errorMessage:"잘못된 접근입니다."
+      })
+    }
+    await Posts.updateOne({title: title},{content: content})
+        res.json({success: true})
+  } catch (err) {
+    console.log("잘못된 접근입니다.")
+  } 
 })
+
 
 // router.put("/posts/:_id", async(req, res) => {
 //   const {_id} = req.params
